480f4226bf84eaab9708ec054db18ed9f7c5e0ce
alirezaghd <45558196+alirezaghd@users.noreply.github.com>
2021-04-07 18:49:20 +0430

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-def read_():
+def read_E_To_P():
     #int
     print('loading')
     f = open('alireza_translate/translate.txt')
@@ -26,16 +26,62 @@ def read_():
 
     user_string = input()
 
+    user_sen = user_string.split('.')
     user_word = user_string.split(' ')
+    
+    #####################################
 
-    for j in range(len(user_word)):
-        for i in range(len(words)):
-            if words[i]['english'] == user_word[j] :
-                print(words[i]["persian"])
+    if '.' in user_string:
+        for j in range(len(user_sen)):
+            for i in range(len(words)):
+                if words[i]['english'] == user_sen[j] :
+                    print(words[i]["persian"])
+                    
+
+    elif ' ' in user_string :               
+        for j in range(len(user_word)):
+            for i in range(len(words)):
+                if words[i]['english'] == user_word[j] :
+                    print(words[i]["persian"])
+                    break
+            else:    
+                print(user_word[j],end=' ')
+
+def read_P_To_E():
+    #int
+    print('loading')
+    f2 = open('alireza_translate/translate.txt')
+
+    big_text1 = f2.read()
+
+    parts1 = big_text1.split('\n')
+
+    words1 = []
+    i = 0
+
+    while i<len(parts1):
+
+        my_dict1 = {'persian': parts1[i+1] , 'english':parts1[i]} #empty dic
+
+        words1.append(my_dict1)
+
+        i += 2
+
+
+    print('data loaded')
+    print('Welcome Dear user, please enter your text')
+
+    user_string1 = input()
+
+    user_word1 = user_string1.split(' ')
+
+    for j in range(len(user_word1)):
+        for i in range(len(words1)):
+            if words1[i]['persian'] == user_word1[j] :
+                print(words1[i]["english"])
                 break
         else:    
-            print(user_word[j],end=' ')
-
+            print(user_word1[j],end=' ')
 
 def write_():
     print('loading')
@@ -54,14 +100,17 @@ def write_():
 
 
 
+print("Wellcome to Alireza Translate")
+print(" 1- Add New word \n" , "2- translate Engilish to persin \n" , "3- translate persian to Engilish \n ", "4- Exit")
 
-print("1- read transalte" , "2- write translate" , "3- Exit")
 
 m = int(input('insert number of menu : '))
 
-if m == 1:
-    read_()
-if m == 2 :
+if m == 1 :
     write_()
-if m == 3 :
+if m == 2:
+    read_E_To_P()
+if m == 3:
+    read_P_To_E()
+if m == 4 :
     exit()
